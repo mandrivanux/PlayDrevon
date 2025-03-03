83f876abbbc40746be48f7bd8c7dad9c4164eec3
Nozar Mandriva <154652119+mandrivanux@users.noreply.github.com>
2025-03-03 09:49:31 +0800

    PlayDrevonHome.js

--- PlayDrevonHome.js
+++ PlayDrevonHome.js
@@ -1,13 +1,1 @@
-document
-  .getElementById("contactForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-    const mailtoLink = `mailto:smartfrendriva@gmail.com?subject=New%20Contact%20Form%20Submission&body=Name:%20${name}%0AEmail:%20${email}%0AMessage:%20${message}`;
-
-    window.location.href = mailtoLink;
-  });
+document.getElementById("contactForm").addEventListener("submit",function(event){event.preventDefault();const name=document.getElementById("name").value;const email=document.getElementById("email").value;const message=document.getElementById("message").value;const mailtoLink=`mailto:smartfrendriva@gmail.com?subject=New%20Contact%20Form%20Submission&body=Name:%20${name}%0AEmail:%20${email}%0AMessage:%20${message}`;window.location.href=mailtoLink})// code by @mandrivanuzz
